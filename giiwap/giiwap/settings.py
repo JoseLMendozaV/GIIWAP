d7b0a1ed0671e48c5a1939014c6d68697a0c2ed6
José Mendoza <sagaka1g@gmail.com>
2024-12-05 23:03:29 -0500

    Update vercel
    added some updates for vercel operative

--- giiwap/giiwap/settings.py
+++ giiwap/giiwap/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "sdDADAFAFADADAd"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'giiwap-sagaka.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition

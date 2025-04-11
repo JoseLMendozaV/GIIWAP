e1f8840bf4d2f60cd0cc98b7be25ede185943afe
José Mendoza <sagaka1g@gmail.com>
2025-04-11 09:07:57 -0500

    Added functionality for analytics data

--- giiwap/giiwap/settings.py
+++ giiwap/giiwap/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'main.middleware.AnalyticsMiddleware',
     'widget_tweaks',
     'main',
 ]

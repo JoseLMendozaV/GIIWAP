445d64d9a967f249faf21373ee436e7dbcd70b66
José Mendoza <sagaka1g@gmail.com>
2025-04-11 09:23:15 -0500

    FIxed Analytics data

--- giiwap/giiwap/settings.py
+++ giiwap/giiwap/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'main.middleware.AnalyticsMiddleware',
     'widget_tweaks',
     'main',
 ]
